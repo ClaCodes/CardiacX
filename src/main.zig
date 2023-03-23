3e821504c684d6408efc9ede135e950b237c4288
Cla <clamattia@gmail.com>
2023-03-23 22:52:26 +0100

    implement subtraction

--- src/main.zig
+++ src/main.zig
@@ -25,23 +25,13 @@ const CARDIAC = struct {
 
             switch (op_code) {
                 .INP => {unreachable;},
-                .CLA => {
-                    self.accumullator = self.memory[memory_address];
-
-                    },
-                .ADD => {
-                    self.accumullator += self.memory[memory_address];
-
-                    },
-                .TAC => {
-
-                    },
+                .CLA => {self.accumullator = self.memory[memory_address]; },
+                .ADD => {self.accumullator += self.memory[memory_address];},
+                .TAC => {unreachable;},
                 .SFT => {unreachable;},
                 .OUT => {unreachable;},
-                .STO => {
-                    self.memory[memory_address] = self.accumullator;
-                },
-                .SUB => {unreachable;},
+                .STO => {self.memory[memory_address] = self.accumullator;},
+                .SUB => {self.accumullator -= self.memory[memory_address];},
                 .JMP => {unreachable;},
                 .HRS => {break: running;},
             }
@@ -66,22 +56,46 @@ const CARDIAC = struct {
 pub fn main() !void {
 }
 
-test "simple test" {
+test "subtract 7 from 32 expect 25" {
     var mem: [100] u64 = undefined;
 
+    // clear and add *98
     mem[0] = 198;
-    mem[98] = 2;
+    mem[98] = 32;
 
-    mem[1] = 297;
-    mem[97] = 4;
+    // subtract *97
+    mem[1] = 797;
+    mem[97] = 7;
 
+    // store *96
     mem[2] = 696;
 
+    //halt
     mem[3] = 999;
 
     var cardiac = CARDIAC.new(mem);
-
     cardiac.run();
+    try std.testing.expectEqual(cardiac.memory[96], 25);
+}
 
+test "add 4 to 2 expect 6" {
+    var mem: [100] u64 = undefined;
+
+    // clear and add *98
+    mem[0] = 198;
+    mem[98] = 2;
+
+    // add *97
+    mem[1] = 297;
+    mem[97] = 4;
+
+    // store *96
+    mem[2] = 696;
+
+    //halt
+    mem[3] = 999;
+
+    var cardiac = CARDIAC.new(mem);
+    cardiac.run();
     try std.testing.expectEqual(cardiac.memory[96], 6);
 }
