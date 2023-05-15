cad2c3ff9a3f4ac7afd2f2eb5c7c020124fb7d1e
Thomas Stauffer <thomasstauffer@gmx.ch>
2023-05-15 19:52:08 +0200

    subroutine mul2 print: simplify and add comments

--- cardiac_programs/subroutine_mul2_print.asm
+++ cardiac_programs/subroutine_mul2_print.asm
@@ -1,32 +1,32 @@
 CLA A
-STO P
+STO P // store parameter for subroutine
 JMP MUL2
 JMP PRINT
 CLA B
-STO P
+STO P // store parameter for subroutine
 JMP MUL2
 JMP PRINT
 HRS
 
+// doubles the parameter P
 MUL2:
-CLA P
-ADD P
-STO P
-CLA 99
+CLA P // load parameter
+ADD P // add parameter again (aka doubling)
+STO P // store parameter
+CLA 99 // load return address
 STO MUL2RET
 MUL2RET:
 0
 
+// outputs the parameter P
 PRINT:
-CLA P
-STO T
-OUT T
-CLA 99
+OUT P // output parameter
+CLA 99 // load return address
 STO PRINTRET
 PRINTRET:
 0
 
 A: 3
 B: 5
-P: 0
-T: 0
+P: 0 // parameter for subroutines
+T: 0 // temporary for subroutines
